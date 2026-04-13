08ee771d8d2a197d82a3948ab57f50258e14d984
Zoya <zoyaa27@nycstudents.net>
2026-04-13 01:01:36 -0400

    urdvsihkj

--- tool/my_game_folder/game.js
+++ tool/my_game_folder/game.js
@@ -232,5 +232,8 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    backgroundColor: '#000',
     scene: [MenuScene, QuizScene, ScoreScene],
 };
+
+const game = new Phaser.Game(config);
